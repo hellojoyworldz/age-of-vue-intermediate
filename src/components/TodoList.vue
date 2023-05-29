6dfab4cbbc9790bcf0ef3ce2b44953fee9d4ca34
hellojoyworldz <hellojoyworldz@gmail.com>
2023-05-30 00:23:39 +0900

    1 ~ 2-8 강의완료

---
+++ src/components/TodoList.vue
@@ -0,0 +1,97 @@
+<template>
+    <div>
+      <ul>
+        <!-- v-bind:key="중복되지 않을 유일한 값을 key로" -> v-for의 성능을 가속화 -->
+        <li
+          v-for="(todoItem,index) in todoItems"
+          v-bind:key="todoItem"
+          class="shadow"
+        >
+          <i class="checkBtn fas fa-check"></i>
+          {{ todoItem }}
+          <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+            <i class="fa fa-trash" aria-hidden="true"></i>
+          </span>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data: function() {
+      return {
+        todoItems: []
+      };
+    },
+    methods: {
+      removeTodo: function(todoItem, index) {
+        localStorage.removeItem(todoItem) // key를 지움
+        this.todoItems.splice(index, 1) // 새로운 배열을 반환
+        //MDN splice() API 문서 - 자바스크립트 배열 api. 특정 인덱스에서 하나를 지울 수 있다
+        //https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+      }
+    },
+    // vue 라이프사이클
+    created: function() {
+      // 인스턴스가 생성되자마자 호출되는 라이프사이클 훅
+      // 훅 : 생성되는 시점에 안에 로직이 호출된다.
+      if (localStorage.length > 0) {
+        for (var i = 0; i < localStorage.length; i++) {
+          if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+            this.todoItems.push(localStorage.key(i));
+            //console.log(localStorage.key(i));
+          }
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0px;
+    text-align: left;
+  }
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: #fff;
+    border-radius: 5px;
+  }
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+  .checkBtnCompleted {
+    color: #b3adad;
+  }
+  .textCompleted {
+    text-decoration: line-through;
+    color: #b3adad;
+  }
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
+  .checkBtn{
+  line-height: 45px;
+  color:#62acde;
+  margin-right: 5px;
+  }
+  .checkBtnCompleted{
+    color:#b3adad
+  }
+  .textCompleted{
+    text-decoration:line-througth;
+    color:#b3adad;
+  }
+  </style>
+  
