5ab6dff8d238a74d0298a69ceffd9dee5eab5a66
hellojoyworldz <hellojoyworldz@gmail.com>
2023-05-30 15:09:16 +0900

    2-8 ~ 2-11 강의완료

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,62 +1,69 @@
 <template>
-    <div class="inputBox shadow">
-      <!-- v-model :  input에 입력된 text값을 동적으로 뷰 인스턴스 안에 맵핑
-      뷰 안에서의 인스턴스 데이터 변경(스크립트) - 화면에서의 돔조작(데이터 변경) 동일하게 동기화
-      -->
-      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
-      <span class="addContainer" v-on:click="addTodo">
-        <i class="fa fa-plus" aria-hidden="true"></i>
-      </span>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data: function () {
-      return {
-        newTodoItem: "",
-      };
-    },
-    methods: {
-      addTodo: function () {
-        //console.log(this.newTodoItem);
-        // this 일반적으로 js 스코프 지칭
-        // 이 this는? 2-4 강의. 같은 인스턴스를 가르킴
-  
-        // 저장하는 로직
-        // 로컬 스토리지 setItem() API 공식 가이드
-        // https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
-        // 로컬스토리지는 크롬 Application > Storage > Local Storage
-        localStorage.setItem(this.newTodoItem, this.newTodoItem);
+  <div class="inputBox shadow">
+    <!-- v-model :  input에 입력된 text값을 동적으로 뷰 인스턴스 안에 맵핑
+    뷰 안에서의 인스턴스 데이터 변경(스크립트) - 화면에서의 돔조작(데이터 변경) 동일하게 동기화
+    -->
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="fa fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo: function () {
+      //console.log(this.newTodoItem);
+      // this 일반적으로 js 스코프 지칭
+      // 이 this는? 2-4 강의. 같은 인스턴스를 가르킴
+
+      // 저장하는 로직
+      // 로컬 스토리지 setItem() API 공식 가이드
+      // https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
+      // 로컬스토리지는 크롬 Application > Storage > Local Storage
+      if(this.newTodoItem !==''){
+        var obj = {completed:false,item:this.newTodoItem}
+        localStorage.setItem(this.newTodoItem,JSON.stringify(obj)); // obj 객체를 string으로 변환해주는 api
+        //localStorage.setItem(this.newTodoItem, obj);  // obj를 바로 넣으면 [object Object] 로 값을 못읽는다. 
+        // https://medium.com/오늘의-프로그래밍/자바스크립트에서-object-object-가-대체-뭘까-fe55b754e709
+
+        //localStorage.setItem(this.newTodoItem, this.newTodoItem);
         this.clearInput();
-      },
-      clearInput: function () {
-        this.newTodoItem = "";
-      },
+      }
     },
-  };
-  </script>
-  
-  <style scoped>
-  .inputBox {
-    background: white;
-    height: 50px;
-    line-height: 50px;
-    border-radius: 5px;
-  }
-  .inputBox input {
-    border-style: none;
-    font-size: 0.9rem;
-  }
-  .addContainer {
-    float: right;
-    background: linear-gradient(to right, #6478fb, #8763fb);
-    display: block;
-    width: 3rem;
-    border-radius: 0px 5px 5px 0;
-  }
-  .addBtn {
-    color: white;
-    vertical-align: middle;
-  }
-  </style>
+    clearInput: function () {
+      this.newTodoItem = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0px 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,11 +4,14 @@
         <!-- v-bind:key="중복되지 않을 유일한 값을 key로" -> v-for의 성능을 가속화 -->
         <li
           v-for="(todoItem,index) in todoItems"
-          v-bind:key="todoItem"
+          v-bind:key="todoItem.item"
           class="shadow"
         >
-          <i class="checkBtn fas fa-check"></i>
-          {{ todoItem }}
+          <i class="checkBtn fa fa-check" 
+              v-bind:class="{checkBtnCompleted : todoItem.completed}" 
+              v-on:click="toggleComplete(todoItem, index)"
+          ></i>
+          <span v-bind:class="{textCompleted : todoItem.completed}">{{ todoItem.item }}</span>
           <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
             <i class="fa fa-trash" aria-hidden="true"></i>
           </span>
@@ -26,10 +29,16 @@
     },
     methods: {
       removeTodo: function(todoItem, index) {
-        localStorage.removeItem(todoItem) // key를 지움
+        localStorage.removeItem(todoItem.item) // key를 지움
         this.todoItems.splice(index, 1) // 새로운 배열을 반환
         //MDN splice() API 문서 - 자바스크립트 배열 api. 특정 인덱스에서 하나를 지울 수 있다
         //https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+      },
+      toggleComplete:function(todoItem,index){
+        todoItem.completed = !todoItem.completed
+        // 로컬 스토리지의 데이터를 갱신
+        localStorage.removeItem(todoItem.item)
+        localStorage.setItem(todoItem.item, JSON.stringify(todoItem))
       }
     },
     // vue 라이프사이클
@@ -39,8 +48,9 @@
       if (localStorage.length > 0) {
         for (var i = 0; i < localStorage.length; i++) {
           if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+            this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i)))) // type of localStorage.key(i) 는 string
             this.todoItems.push(localStorage.key(i));
-            //console.log(localStorage.key(i));
+           
           }
         }
       }
