45c0541e3f647471816d7572227df01cb4c84b65
hellojoyworldz <hellojoyworldz@gmail.com>
2023-06-16 15:37:42 +0900

    속성 파일로 나누기

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,13 @@
 // node_modules 밑에
 import Vue from "vue";
 import Vuex from "vuex";
+import * as getters from "./getters";
+import * as mutations from "./mutations"; // export한 내용 다 담기
 
 Vue.use(Vuex); // use : vue 플러그인. 전역으로 특정 기능을 추가하고 싶을 떄
 
 const storage = {
   fetch() {
-    // fetch: function()
     const arr = [];
     if (localStorage.length > 0) {
       for (let i = 0; i < localStorage.length; i++) {
@@ -21,38 +22,9 @@ const storage = {
 };
 
 export const store = new Vuex.Store({
-  // export const 변수 다른곳에서 사용 가능
   state: {
-    todoItems: storage.fetch(), //this.$store.state.todoItems
+    todoItems: storage.fetch(),
   },
-  getters: {
-    storedTodoItems(state) {
-      return state.todoItems;
-    },
-  },
-  mutations: {
-    // method
-    addOneItem(state, todoItem) {
-      const obj = { completed: false, item: todoItem };
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      state.todoItems.push(obj); //this X, state O
-    },
-    removeOneItem(state, payload) {
-      localStorage.removeItem(payload.todoItem.item);
-      state.todoItems.splice(payload.index, 1);
-    },
-    toggleOneItem(state, payload) {
-      state.todoItems[payload.index].completed =
-        !state.todoItems[payload.index].completed;
-      localStorage.removeItem(payload.todoItem.item);
-      localStorage.setItem(
-        payload.todoItem.item,
-        JSON.stringify(payload.todoItem)
-      );
-    },
-    cleanAll(state) {
-      localStorage.clear();
-      state.todoItems = [];
-    },
-  },
+  getters,
+  mutations,
 });
